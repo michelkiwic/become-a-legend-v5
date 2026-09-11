dfe25e6ed24065ab3c9329f63a9c89bacd3a6267
michelkiwic <michelkiwic@users.noreply.github.com>
2026-09-11 14:36:37 +0200

    Create Yoshi Moshi V5

--- vite.github.config.ts
+++ vite.github.config.ts
@@ -2,7 +2,7 @@ import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  base: "/become-a-legend-v4/",
+  base: "/become-a-legend-v5/",
   plugins: [react()],
   build: {
     outDir: "github-pages-dist",
